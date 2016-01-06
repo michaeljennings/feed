f977efeb51aea4d1d9009a261dbc9edc1ac42fc6
Michael Jennings <michael.jennings91@gmail.com>
2016-01-06 15:45:32 +0000

    Add read and unread methods to the notification repository

--- src/Notifications/Repository.php
+++ src/Notifications/Repository.php
@@ -2,9 +2,11 @@
 
 namespace Michaeljennings\Feed\Notifications;
 
-use Michaeljennings\Feed\Contracts\Repository as RepositoryContracts;
+use Carbon\Carbon;
+use Michaeljennings\Feed\Contracts\Notification as NotificationContract;
+use Michaeljennings\Feed\Contracts\Repository as RepositoryContract;
 
-class Repository implements RepositoryContracts
+class Repository implements RepositoryContract
 {
     /**
      * Create a new notification.
@@ -18,6 +20,8 @@ class Repository implements RepositoryContracts
     }
 
     /**
+     * Get all of the unread notifications for the notifiable members.
+     *
      * @param array $types
      * @param array $ids
      * @param null  $limit
@@ -39,5 +43,59 @@ class Repository implements RepositoryContracts
         }
 
         return $query->get();
+    }
+
+    /**
+     * Get all of the read notifications for the notifiable members.
+     *
+     * @param array $types
+     * @param array $ids
+     * @param null  $limit
+     * @param null  $offset
+     * @return mixed
+     */
+    public function getReadNotifications(array $types, array $ids, $limit = null, $offset = null)
+    {
+        $query = Notification::whereIn('notifiable_type', $types)
+                             ->whereIn('notifiable_id', $ids)
+                             ->where('read', true);
+
+        if ($limit) {
+            $query->limit($limit);
+        }
+
+        if ($offset) {
+            $query->offset($offset);
+        }
+
+        return $query->get();
+    }
+
+    /**
+     * Mark the notification as read.
+     *
+     * @param NotificationContract $notification
+     * @return mixed
+     */
+    public function read(NotificationContract $notification)
+    {
+        $notification->read = true;
+        $notification->read_at = new Carbon();
+
+        return $notification->save();
+    }
+
+    /**
+     * Mark the notification as unread.
+     *
+     * @param NotificationContract $notification
+     * @return mixed
+     */
+    public function unread(NotificationContract $notification)
+    {
+        $notification->read = false;
+        $notification->read_at = null;
+
+        return $notification->save();
     }
 }
