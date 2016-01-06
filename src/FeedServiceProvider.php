550ed25857dd062c3839e40ec8a11fbb3462827f
Michael Jennings <michael.jennings91@gmail.com>
2016-01-06 18:11:02 +0000

    Add service provider

---
+++ src/FeedServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Michaeljennings\Feed;
+
+use Illuminate\Support\ServiceProvider;
+
+class FeedServiceProvider extends ServiceProvider
+{
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+        $this->app->bind('michaeljennings.feed.repository', 'Michaeljennings\Feed\Notifications\Repository');
+
+        $this->app->bind('michaeljennings.feed', function($app) {
+            return new Feed($app['michaeljennings.feed.repository']);
+        });
+
+        $this->app->alias('michaeljennings.feed.repository', 'Michaeljennings\Feed\Contracts\Repository');
+        $this->app->alias('michaeljennings.feed', 'Michaeljennings\Feed\Contracts\PullFeed');
+        $this->app->alias('michaeljennings.feed', 'Michaeljennings\Feed\Contracts\PushFeed');
+    }
+}
