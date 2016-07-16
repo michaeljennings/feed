e879fee9143b15ac7d5df65884bd84b4ba23c5be
Michael Jennings <michael.jennings91@gmail.com>
2016-07-16 14:10:09 +0100

    Add stubs to the store contract

--- src/Contracts/Store.php
+++ src/Contracts/Store.php
@@ -4,5 +4,57 @@ namespace Michaeljennings\Feed\Contracts;
 
 interface Store
 {
+    /**
+     * Get all of the unread notifications where their notifiable type
+     * is in the array of types, and their notifiable id is in the
+     * array of ids.
+     *
+     * @param array $types
+     * @param array $ids
+     * @return Notification[]
+     */
+    public function getNotifications(array $types, array $ids);
 
+    /**
+     * Get all of the read notifications where their notifiable type
+     * is in the array of types, and their notifiable id is in the
+     * array of ids.
+     *
+     * @param array $types
+     * @param array $ids
+     * @return Notification[]
+     */
+    public function getReadNotifications(array $types, array $ids);
+
+    /**
+     * Add a limit to the query.
+     *
+     * @param int $limit
+     * @return Store
+     */
+    public function limit($limit);
+
+    /**
+     * Add an offset to the query.
+     *
+     * @param int $offset
+     * @return Store
+     */
+    public function offset($offset);
+
+    /**
+     * Mark the provided notification as read.
+     *
+     * @param Notification|int $notification
+     * @return mixed
+     */
+    public function markAsRead($notification);
+
+    /**
+     * Mark the provided notification as unread.
+     *
+     * @param Notification|int $notification
+     * @return mixed
+     */
+    public function markAsUnread($notification);
 }
