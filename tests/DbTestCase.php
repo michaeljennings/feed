96ce3e3697352971193d9acf2ad4164ec03277e5
Michael Jennings <michaeljennings@gmlconsulting.co.uk>
2018-07-23 13:03:02 +0100

    Update tests to use orchestra testbench

--- tests/DbTestCase.php
+++ tests/DbTestCase.php
@@ -2,28 +2,11 @@
 
 namespace Michaeljennings\Feed\Tests;
 
-use Illuminate\Filesystem\ClassFinder;
-use Illuminate\Filesystem\Filesystem;
-use Illuminate\Foundation\Testing\TestCase;
+use Michaeljennings\Feed\FeedServiceProvider;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
-class DbTestCase extends TestCase
+class DbTestCase extends OrchestraTestCase
 {
-    /**
-     * Boots the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../vendor/laravel/laravel/bootstrap/app.php';
-
-        $app->register('Michaeljennings\Feed\FeedServiceProvider');
-
-        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-        return $app;
-    }
-
     /**
      * Setup DB before each test.
      *
@@ -33,28 +16,33 @@ class DbTestCase extends TestCase
     {
         parent::setUp();
 
-        $this->app['config']->set('database.default','sqlite');
-        $this->app['config']->set('database.connections.sqlite.database', ':memory:');
-
-        $this->migrate();
+        $this->artisan('migrate', ['--path' => '../../../../migrations']);
     }
 
     /**
-     * run package database migrations
+     * Define environment setup.
      *
+     * @param  \Illuminate\Foundation\Application  $app
      * @return void
      */
-    public function migrate()
+    protected function getEnvironmentSetUp($app)
     {
-        $fileSystem = new Filesystem;
-        $classFinder = new ClassFinder;
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
 
-        foreach($fileSystem->files(__DIR__ . "/../migrations") as $file)
-        {
-            $fileSystem->requireOnce($file);
-            $migrationClass = $classFinder->findClass($file);
-
-            (new $migrationClass)->up();
-        }
+    /**
+     * @inheritdoc
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            FeedServiceProvider::class,
+        ];
     }
 }
